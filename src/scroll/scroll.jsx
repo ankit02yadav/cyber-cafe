58fb691e9acae3a5f4f635c0869baa78e3b51933
Ankit Yadav <yadavankit02062005@gmail.com>
2025-03-10 22:09:18 +0530

    Update scroll.jsx

--- src/scroll/scroll.jsx
+++ src/scroll/scroll.jsx
@@ -3,9 +3,9 @@ function Scroll(){
     return(
         <div className="scroll-text">
             <div className="texxt">
-            <span className='text-scroll'>GAUTAM DOCS. </span>
-            <span className='text-scroll'>GAUTAM DOCS. </span>
-            <span className='text-scroll'>GAUTAM DOCS. </span>
+            <span className='text-scroll'>VERMA DOCS. </span>
+            <span className='text-scroll'>VERMA DOCS. </span>
+            <span className='text-scroll'>VERMA DOCS. </span>
             </div>
         </div>
     );
